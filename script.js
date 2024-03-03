6c5054d0bd3e796d3661acb1f69b057fb2846ea0
gastbr <71907959+gastbr@users.noreply.github.com>
2024-03-03 17:55:24 +0000

    Pendiente JS, spinners y pagination.

--- script.js
+++ script.js
@@ -63,3 +63,25 @@ function myFunction() {
 }
 
 document.getElementById("progress").style.height = "5px";
+
+// BS5 Alert
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Se ha abierto un video.', 'success')
+  })
+}
